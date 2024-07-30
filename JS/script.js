2470408f5b7336efd1ccc7c4b325d41e3a744294
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-07-30 15:06:53 -0400

    Name and Last name

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,11 @@ console.log(fineNumber, typeof fineNumber)
 /*--------------------------
       FASE RACCOLTA DATI
 --------------------------*/
+
+// 1-Chiedi all'utente il suo nome
+const firstName = prompt('Inserisci il tuo primo nome');
+console.log(firstName)
+
+// 2-Chiedi all'utente il suo nome
+const lastName = prompt('Inserisci il tuo cognome');
+console.log(lastName)
