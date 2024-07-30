fa4fb479f2b73774556f1351cdb4a35d04547db8
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-07-30 15:00:35 -0400

    Variable 21

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
-console.log('Hi World!')
+/*--------------------------
+      FASE PREPARATORIA
+--------------------------*/
+
+// Preparo la variabile che appare nel fine della Password
+const fineNumber = 21;
+console.log(fineNumber, typeof fineNumber)
+
+
+/*--------------------------
+      FASE RACCOLTA DATI
+--------------------------*/
